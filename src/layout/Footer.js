be35a01dd8b37ffe1fea37d3ba9d258e2cf229c4
jideabdqudus <jideabdqudus@gmail.com>
2020-10-26 14:30:57 +0100

    Add Commit

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Divider } from "antd";
 import Logo from "../assets/lo12go.png";
+import {
+  MailOutlined,
+  FacebookFilled,
+  TwitterCircleFilled,
+  InstagramFilled,
+  LinkedinFilled,
+} from "@ant-design/icons";
 
 const Footer = () => {
   return (
     <div className="container">
-      <Row style={{paddingTop:"60px"}}>
+      <Row style={{ paddingTop: "60px" }}>
         <Col span={12}>
           <Row>
             <Col span={24}>
@@ -26,8 +33,81 @@ const Footer = () => {
             </Col>
           </Row>
         </Col>
-        <Col span={4}>asaasa</Col>
-        <Col span={8}>asasa</Col>
+        <Col span={6}>
+          <Row>
+            <Col span={24}>
+              <p style={{ color: "white", marginBottom: "30px" }}>
+                Our Corporate Office:
+              </p>
+            </Col>
+            <Col span={24}>
+              <p
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "20px",
+                  marginBottom: 0,
+                  color: "white",
+                }}
+              >
+                Vibranium Valley
+              </p>
+            </Col>
+            <Col span={24}>
+              <p
+                style={{
+                  fontWeight: "lighter",
+
+                  color: "white",
+                }}
+              >
+                42, Local Airport Road, Ikeja, Lagos
+              </p>
+            </Col>
+          </Row>
+        </Col>
+        <Col span={6}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2762.538213155658!2d3.326359579846835!3d6.566419835487606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sng!4v1603718916819!5m2!1sen!2sng"
+            width="600"
+            height="450"
+            frameborder="0"
+            style="border:0;"
+            allowfullscreen=""
+            aria-hidden="false"
+            tabindex="0"
+          ></iframe>
+        </Col>
+      </Row>
+      <Divider />
+      <Row>
+        <Col span={18}>
+          <Row>
+            <MailOutlined
+              style={{
+                color: "white",
+                fontSize: "20px",
+                marginRight: "10px",
+              }}
+            />{" "}
+            <p style={{ color: "white" }}> vgg@venturegardengroup.com</p>
+          </Row>
+        </Col>
+        <Col span={6}>
+          <Row>
+            <FacebookFilled
+              style={{ fontSize: "25px", paddingRight: "25px", color: "white" }}
+            />
+            <TwitterCircleFilled
+              style={{ fontSize: "25px", paddingRight: "25px", color: "white" }}
+            />
+            <InstagramFilled
+              style={{ fontSize: "25px", paddingRight: "25px", color: "white" }}
+            />
+            <LinkedinFilled
+              style={{ fontSize: "25px", paddingRight: "25px", color: "white" }}
+            />
+          </Row>
+        </Col>
       </Row>
     </div>
   );
