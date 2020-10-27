ef5214fa8c2fd2ad0197a49cceb20d82cc825dd6
jideabdqudus <jideabdqudus@gmail.com>
2020-10-27 11:54:32 +0100

    Add Commit

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -66,16 +66,7 @@ const Footer = () => {
           </Row>
         </Col>
         <Col span={6}>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2762.538213155658!2d3.326359579846835!3d6.566419835487606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sng!4v1603718916819!5m2!1sen!2sng"
-            width="600"
-            height="450"
-            frameborder="0"
-            style="border:0;"
-            allowfullscreen=""
-            aria-hidden="false"
-            tabindex="0"
-          ></iframe>
+        
         </Col>
       </Row>
       <Divider />
